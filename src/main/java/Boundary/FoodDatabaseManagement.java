c190c4ac235d7fa50820cb0633c3bf303c10cd86
IHotop7 <Ihotop21@gmail.com>
2024-04-23 15:30:54 -0700

    commit

--- src/main/java/Boundary/FoodDatabaseManagement.java
+++ src/main/java/Boundary/FoodDatabaseManagement.java
@@ -9,34 +9,63 @@ import java.sql.*;
  * @author Isaac Hotop
  */
 public class FoodDatabaseManagement {
-    public FoodDatabaseManagement() {
-
-    }
-
-
-    /**
-     * connectDatabase
-     * Loads the JDBC driver then uses the user's username and password to connect to the database
-     *
-     * @param username The user's username
-     * @param password The user's password
-     */
-    public void connectDatabase(String username, String password) throws ClassNotFoundException {
-        /*
+    Connection connection;
+    public FoodDatabaseManagement() throws ClassNotFoundException {
+         /*
         Load JDBC driver
-        You can download this driver from thi link: https://dev.mysql.com/downloads/connector/j/
+        You can download this driver from this link: https://dev.mysql.com/downloads/connector/j/
         Then add the jar file to the project's dependencies
         the java.sql.* import will not work until you do this
          */
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         try {
-                                                                                     //host:port/Database name
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodDatabase", username, password);
+            //host:port/Database name  root username, root password
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "Changeme1!");
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * signInUser
+     * Retrives the user's data from the database and creates a User object to store the data in.
+     * @param username
+     * @param password
+     * @return
+     * @throws ClassNotFoundException
+     */
+    public boolean signInUser(String username, String password) throws ClassNotFoundException {
+
+        return false;
+    }
+
+    /**
+     * addUser
+     * Creates a query using the given username and password to add a user and their associated table to the database
+     *
+     * @param username The user's username
+     * @param password The user's password
+     * @return true if the user and table was successfully added return false if not
+     */
+    public boolean addUser(String username, String password) {
+        try {
+            PreparedStatement addUser = connection.prepareStatement("CREATE USER ?@'localhost' IDENTIFIED BY ?");
+            addUser.setString(1,username);
+            addUser.setString(2, password);
+            addUser.executeUpdate();
+
+            PreparedStatement addUserTable = connection.prepareStatement("CREATE TABLE "+username+"_table (id INT AUTO_INCREMENT PRIMARY KEY, food_date DATE, food VARCHAR(255))");
+            addUserTable.executeUpdate();
+
+            return true;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
     }
 }
--- src/main/java/Boundary/SignIn.java
+++ src/main/java/Boundary/SignIn.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
 
 /**
  * @author Isaac Hotop
@@ -22,49 +23,103 @@ import java.io.IOException;
 public class SignIn extends Application {
     @Override
     public void start(Stage stage) throws IOException, ClassNotFoundException {
+        InputValidation inputValidation = new InputValidation();
+        FoodDatabaseManagement foodDatabaseManagement = new FoodDatabaseManagement();
 
-        //Create signIn page
+        //Create signIn pane
         GridPane signInPane = new GridPane();
         HBox hBox = new HBox();
         signInPane.setAlignment(Pos.CENTER);
         hBox.setAlignment(Pos.CENTER);
         signInPane.add(hBox, 0, 0);
 
-        //Create sign in fields and buttons and add them to SignInPage
+        //Create addUser pane
+        GridPane addUserPane = new GridPane();
+        HBox addUserHbox = new HBox();
+        addUserPane.setAlignment(Pos.CENTER);
+        addUserHbox.setAlignment(Pos.CENTER);
+        addUserPane.add(addUserHbox, 1, 1);
+
+        //Create sign in fields and buttons and add them to SignInPane
         TextField usrNameField = new TextField();
         TextField passwdField = new TextField();
         Button signInBtn = new Button("Sign In");
+        Button addUserBtn = new Button("Create New User");
         Label status = new Label("Enter your username and password above");
         usrNameField.setPromptText("Username:");
         passwdField.setPromptText("Password:");
         hBox.getChildren().addAll(usrNameField, passwdField);
         signInPane.add(signInBtn, 0, 1);
-        signInPane.add(status, 0, 2);
+        signInPane.add(addUserBtn, 0,2);
+        signInPane.add(status, 0, 3);
         signInPane.setHalignment(signInBtn, HPos.CENTER);
+        signInPane.setHalignment(addUserBtn, HPos.CENTER);
         signInPane.setHalignment(status, HPos.CENTER);
 
-        //gets credentials from the textFields, validates them, then attempts a connection to the database when signinBtn is clicked
-        signInBtn.setOnAction(e -> {
-            InputValidation inputValidation = new InputValidation();
-            FoodDatabaseManagement foodDatabaseManagement = new FoodDatabaseManagement();
-
-            if (inputValidation.UsernamePasswordValidation(usrNameField.getText(), passwdField.getText())) {
-                try {
-                    foodDatabaseManagement.connectDatabase(usrNameField.getText(), passwdField.getText());
-                }
-                catch (ClassNotFoundException e1) {
-                    throw new RuntimeException(e1);
-                }
-            }
-            else {
-                status.setText("Login Failed...");
-            }
-        });
+        //Create add user fields and buttons and add them to addUserPane
+        TextField addUsrNamefield = new TextField();
+        TextField addPasswdField = new TextField();
+        TextField passwdConfirmation = new TextField();
+        Button createUserBtn = new Button("Create User");
+        Button backBtn = new Button("Back to Sign in Page");
+        addUsrNamefield.setPromptText("Username:");
+        addPasswdField.setPromptText("Password:");
+        passwdConfirmation.setPromptText("Confirm Password:");
+        addUserHbox.getChildren().addAll(addPasswdField, passwdConfirmation);
+        addUserPane.add(addUsrNamefield,1,0);
+        addUserPane.add(status,1,2);
+        addUserPane.add(backBtn,0,3);
+        addUserPane.add(createUserBtn,2,3);
 
         //Sets the scene as SignInPane
         Scene scene = new Scene(signInPane, 550, 300);
         stage.setTitle("Calorie Tracker");
         stage.setScene(scene);
         stage.show();
+
+        //gets credentials from the textFields, validates them, then attempts a connection to the database when signInBtn is clicked
+        signInBtn.setOnAction(e -> {
+
+
+            if (inputValidation.UsernamePasswordValidation(usrNameField.getText(), passwdField.getText())) {
+
+            }
+            else {
+                status.setText("Login Failed...");
+            }
+        });
+
+        //sets the root of the scene to addUserPane when addUserBtn is clicked
+        addUserBtn.setOnAction(e -> {
+            scene.setRoot(addUserPane);
+        });
+
+        //resets the scene's root back to SignInPane when backBtn is clicked
+        backBtn.setOnAction(e -> {
+            scene.setRoot(signInPane);
+        });
+
+        //Gets credentials from the textFields, validates them, then creates a query to add the user and their
+        //associated table to the database when the createUserBtn is clicked
+        createUserBtn.setOnAction(e -> {
+            if(addPasswdField.getText().equals(passwdConfirmation.getText())) {
+                if (inputValidation.UsernamePasswordValidation(addUsrNamefield.getText(), addPasswdField.getText())) {
+                        if(foodDatabaseManagement.addUser(addUsrNamefield.getText(), addPasswdField.getText())) {
+                            status.setText(addUsrNamefield.getText()+" was added");
+                        }
+                        else {
+                            status.setText("Could not add user...");
+                        }
+                }
+                else {
+                    status.setText("Don't use these characters: ', \", ;, `, -, _, (), [], *, =, /, \\, %, >, <, $, &, |");
+                }
+            }
+            else {
+                status.setText("Both password fields need to match!");
+            }
+
+        });
+
     }
 }
