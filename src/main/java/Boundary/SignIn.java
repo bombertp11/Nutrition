b2c7390b4c81f144c077d09304ba96d0f88fbc31
IHotop7 <Ihotop21@gmail.com>
2024-04-23 11:45:41 -0700

    commit

--- src/main/java/Boundary/SignIn.java
+++ src/main/java/Boundary/SignIn.java
@@ -1,6 +1,13 @@
 package Boundary;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,5 +21,24 @@ public class SignIn extends Application {
     @Override
     public void start(Stage stage) throws IOException, ClassNotFoundException {
 
+        //Create login page
+        GridPane loginPane = new GridPane();
+        HBox hBox = new HBox();
+        loginPane.setAlignment(Pos.CENTER);
+        hBox.setAlignment(Pos.CENTER);
+        loginPane.add(hBox, 0, 0);
+
+        //Create login fields and buttons and add them to loginPage
+        TextField usrNameField = new TextField();
+        TextField passwdField = new TextField();
+        Button loginBtn = new Button("Login");
+        Label status = new Label("Enter your username and password above");
+        usrNameField.setPromptText("Username:");
+        passwdField.setPromptText("Password:");
+        hBox.getChildren().addAll(usrNameField, passwdField);
+        loginPane.add(loginBtn, 0, 1);
+        loginPane.add(status, 0, 2);
+        loginPane.setHalignment(loginBtn, HPos.CENTER);
+        loginPane.setHalignment(status, HPos.CENTER);
     }
 }
